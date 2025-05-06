d3e8a6bca6dafdedee5ed09618b9844e9d6994d2
马军 <majun@xiwicloud.com>
2025-05-06 20:42:20 +0800

    fix: 加上插件

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import App from './App.vue'
 import { createSSRApp } from 'vue'
-import uView from './uni_modules/vk-uview-ui';
-import * as Pinia from 'pinia';
-import i18n from './locales'
+import uView from '/uni_modules/vk-uview-ui'
+import * as Pinia from 'pinia'
+import i18n from '/locales'
 
-export  function createApp() {
+export function createApp() {
   const app = createSSRApp(App)
-  app.use( Pinia.createPinia())
+  app.use(Pinia.createPinia())
   app.use(i18n)
- // 使用 uView UI
- app.use(uView)
+  // 使用 uView UI
+  app.use(uView)
   return {
     app,
     Pinia
